4d498dd020cf905cfddf4e49101f564887bac416
rayannn <74650337+rrrayan@users.noreply.github.com>
2021-06-13 21:27:24 +0000

    final

--- script.js
+++ script.js
@@ -23,8 +23,8 @@ const GAMEOVER = 2;
 var spelStatus = SPELEN;
 
 
-var spelerX = 200; // x-positie van speler
-var spelerY = 650; // y-positie van speler
+var spelerX = 100; // x-positie van speler
+var spelerY = 100; // y-positie van speler
 
 var kogelX = 0;    // x-positie van kogel
 var kogelY = 0;    // y-positie van kogel
